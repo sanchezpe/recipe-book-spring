88fb42b63027aa1d049a868b26f2cf8e3c78d0cc
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-24 22:42:32 -0500

    initial commit

---
+++ src/main/java/com/example/recipebookspring/RecipeBookSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipebookspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBookSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeBookSpringApplication.class, args);
+    }
+
+}
