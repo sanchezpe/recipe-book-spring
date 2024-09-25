f389375ab6b48fb7c44d354886d9c4e77cedb91b
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-24 22:42:43 -0500

    Batch update recipes REST API

--- src/main/java/com/example/recipebookspring/repository/RecipeRepository.java
+++ src/main/java/com/example/recipebookspring/repository/RecipeRepository.java
@@ -5,6 +5,9 @@ import com.example.recipebookspring.domain.RecipeProjection;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Set;
+
 @RepositoryRestResource(excerptProjection = RecipeProjection.class)
 public interface RecipeRepository extends MongoRepository<Recipe, String> {
+    void deleteAllByIdNotIn(Set<String> incomingRecipeIds);
 }
